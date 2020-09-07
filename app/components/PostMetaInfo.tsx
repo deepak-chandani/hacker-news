f62b973bc83d0bdd319a1c37d1fafdab5d8f02e9
Deepak Chandani <shining.deepak10@gmail.com>
2020-09-07 17:48:41 +0530

    updated PostMetaInfo component

---
+++ app/components/PostMetaInfo.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import { Link } from 'react-router-dom'
+import { formatDate } from '../utils/helpers'
+import {useTheme} from '../contexts/theme'
+import {Post} from "../types/common";
+
+type Props = Omit<Post, 'url' | 'title'>;
+
+const PostMetaInfo:React.FC<Props> = ({ by, time, id, descendants }) => {
+  const {theme} = useTheme();
+
+  return (
+      <div className={`meta-info-${theme}`}>
+        <span>by <Link to={`/user?id=${by}`}>{by}</Link></span>
+        <span>on {formatDate(time)}</span>
+        {typeof descendants === 'number' && (
+          <span>
+            with <Link to={`/post?id=${id}`}>{descendants}</Link> comments
+          </span>
+        )}
+      </div>
+  )
+};
+
+
+export default PostMetaInfo;
